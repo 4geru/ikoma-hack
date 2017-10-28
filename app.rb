601bbbf3c4898543e8341e57e50139fd5bfa2866
Gumio <quran.gumi3134@gmail.com>
2017-10-28 14:26:37 +0900

    修正

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        client.reply_message(event['replyToken'], make_carousel_template_data)
+        client.reply_message(event['replyToken'], make_carousel_template_data())
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
@@ -41,22 +41,58 @@ post '/callback' do
   "OK"
 end
 
-def make_carousel_template_data
+def make_carousel_template_data()
   return message = {
   "type": "template",
-  "altText": "select mission",
+  "altText": "this is a carousel template",
   "template": {
       "type": "carousel",
-      "columns": {
-        "thumbnailImageUrl": "https://wing-auctions.c.yimg.jp/sim?furl=auctions.c.yimg.jp%2Fimages.auctions.yahoo.co.jp%2Fimage%2Fdr000%2Fauc0310%2Fusers%2F3%2F0%2F9%2F2%2Fneutraltakizawa-img640x480-1507701425ktdswi32237.jpg&dc=1&sr.fs=20000",
-        "title": "xxx遺跡",
-        "text": "遺跡って響き...いいよね！！",
-        "actions": {
-          "type": "uri",
-          "label": "View detail",
-          "uri": "http://www2.city.ikoma.lg.jp/dm/12/1206shonotani/120602shonotani/120602shonotani.php"
-        }
-      }
-    }
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
+      ]
   }
+}
 end
