3fc925cc766758cb35bda4d716c1316fea8fd29e
4geru sakisaka <westhouse51@gmail.com>
2017-10-28 06:46:56 +0000

    merge start function

--- app.rb
+++ app.rb
@@ -1,82 +1,10 @@
 require 'sinatra'
 require 'line/bot'
+require 'sinatra/activerecord'
+# require 'sinatra/activerecord/rake'
+require './models'
 require './src/hello'
+require './src/line'
+require './src/web'
 require 'dotenv'
 Dotenv.load
-
-# 微小変更部分！確認用。
-get '/' do
-  "test"
-end
-
-def client
-  @client ||= Line::Bot::Client.new { |config|
-
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
-
-post '/callback' do
-  body = request.body.read
-
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
-
-  events = client.parse_events_from(body)
-  events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        
-        res = client.reply_message(event['replyToken'], make_carousel_template_data)
-        p res
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
-      end
-    end
-  }
-
-  "OK"
-end
-
-def make_carousel_template_data()
-{
-  "type": "template",
-  "altText": "this is a image carousel template",
-  "template": {
-      "type": "image_carousel",
-      "columns": [
-          {
-            "imageUrl": "https://example.com/bot/images/item1.jpg",
-            "action": {
-              "type": "postback",
-              "label": "Buy",
-              "data": "action=buy&itemid=111"
-            }
-          },
-          {
-            "imageUrl": "https://example.com/bot/images/item2.jpg",
-            "action": {
-              "type": "message",
-              "label": "Yes",
-              "text": "yes"
-            }
-          },
-          {
-            "imageUrl": "https://example.com/bot/images/item3.jpg",
-            "action": {
-              "type": "uri",
-              "label": "View detail",
-              "uri": "http://example.com/page/222"
-            }
-          }
-      ]
-  }
-}
-end
