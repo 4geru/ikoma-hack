03e854e7f0d6dc68f6e902178956a61ecf6f1ee4
ShotaOnishi <showta.0804@gmail.com>
2017-10-28 15:08:25 +0900

    fix bug 2

--- src/line.rb
+++ src/line.rb
@@ -48,11 +48,11 @@ post '/callback' do
 
 
 
-          image = Image.new()
-          image.user_id = 1
-          image.all_story_id = 1
-          image.url = result['secure_url']
-          image.save
+          # image = Image.new()
+          # image.user_id = 1
+          # image.all_story_id = 1
+          # image.url = result['secure_url']
+          # image.save
 
           message = [
             {
