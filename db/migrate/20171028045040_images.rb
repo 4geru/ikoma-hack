4344b025a44be724d13eeb9e1e53ab60ff315595
4geru sakisaka <westhouse51@gmail.com>
2017-10-29 11:02:56 +0000

    image -> imagesに変更

--- db/migrate/20171028045040_images.rb
+++ db/migrate/20171028045040_images.rb
@@ -1,10 +1,10 @@
-class Image < ActiveRecord::Migration
+class Images < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.integer :user_id
       t.integer :all_story_id
       t.string  :url
-      t.timestamps
+      t.timestamps :null
     end
 
   end
