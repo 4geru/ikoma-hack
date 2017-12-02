9711f1562c1db0de1d03469a46b02e511cdb8dca
4geru sakisaka <westhouse51@gmail.com>
2017-12-02 16:52:06 +0000

    表現の修正

--- src/picture_book.rb
+++ src/picture_book.rb
@@ -1,11 +1,12 @@
 def picture_book(user_id)
+  photos = User.find(user_id).photos
   {
   "type": "template",
   "altText": "図鑑選択中",
   "template": {
       "type": "buttons",
       "title": "図鑑",
-      "text": "Mapで図鑑を見る",
+      "text": "現在#{photos.length}件の建物が登録できてるよ！\nMapで図鑑を見る",
       "actions": [
           {
             "type": "uri",
